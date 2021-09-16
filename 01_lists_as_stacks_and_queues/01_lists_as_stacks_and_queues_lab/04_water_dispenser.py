fabbb0e9ec5b53fd78626026573fb837be41439c
NPencheva <nvpencheva@gmail.com>
2021-09-16 23:53:14 +0300

    Update

---
+++ 01_lists_as_stacks_and_queues/01_lists_as_stacks_and_queues_lab/04_water_dispenser.py
@@ -0,0 +1,55 @@
+from io import StringIO
+import sys
+
+input1 = """2
+Peter
+Amy
+Start
+2
+refill 1
+1
+End"""
+input2 = """10
+Peter
+George
+Amy
+Alice
+Start
+2
+3
+3
+3
+End"""
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
+
+from collections import deque
+
+people = deque()
+
+starting_quantity = int(input())
+liters_left = starting_quantity
+
+while True:
+    command = input()
+    if command == "Start":
+        break
+    else:
+        people.append(command)
+
+while True:
+    command_new = input()
+    if command_new == "End":
+        break
+    elif command_new.startswith("refill"):
+        to_refill = command_new.split(" ")
+        liters_left += int(to_refill[1])
+    else:
+        liters_needed = int(command_new)
+        if liters_needed <= liters_left:
+            liters_left -= liters_needed
+            print(f"{people.popleft()} got water")
+        else:
+            print(f"{people.popleft()} must wait")
+
+print(f"{liters_left} liters left")
