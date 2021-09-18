b9e66dbd2d0b461d91b4aa3decdf61caddba3398
NPencheva <nvpencheva@gmail.com>
2021-09-18 15:00:01 +0300

    Update

--- 01_lists_as_stacks_and_queues/01_lists_as_stacks_and_queues_lab/03_supermarket.py
+++ 01_lists_as_stacks_and_queues/01_lists_as_stacks_and_queues_lab/03_supermarket.py
@@ -1,5 +1,6 @@
 from io import StringIO
 import sys
+from collections import deque
 
 input1 = """George
 Peter
@@ -16,8 +17,6 @@ Alexander
 End"""
 # sys.stdin = StringIO(input1)
 sys.stdin = StringIO(input2)
-
-from collections import deque
 
 queue = deque()
 
